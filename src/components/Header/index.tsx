8439572a29ff4bdb8a65f63c4d90761683651294
NicolaAK <kolyankalachev@gmail.com>
2023-01-02 21:38:20 +0700

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,7 @@ import { HeaderContainer, HeaderContent } from './styles';
 const Header = () => (
     <HeaderContainer>
         <HeaderContent>Header</HeaderContent>
-
         <h1> fgnfgn</h1>
-
         <h2> fgnfgn</h2>
     </HeaderContainer>
 );
