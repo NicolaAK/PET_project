2f20b7bd106ea32e05915b0456ee4e756d0d6761
NicolaAK <kolyankalachev@gmail.com>
2023-01-15 20:28:09 +0700

    header2

--- src/components/Main/HeaderBox/index.tsx
+++ src/components/Main/HeaderBox/index.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import { WatchProducts, NewCollection, BoxTitle, BoxContainer, BoxFotos } from '@components/Main/HeaderBox/styled';
+import {
+    WatchProducts,
+    NewCollection,
+    BoxTitle,
+    BoxContainer,
+    BoxFotos,
+    FotoModel3,
+    FotoModel1,
+    FotoModel2,
+    WatchProductsContainer,
+    ArrowContainer,
+} from '@components/Main/HeaderBox/styled';
 import Line from '@assets/icons/line.svg';
+import ArrowLight from '@assets/icons/arrowRight.svg';
 import mainModel1 from '@assets/foto/mainModel1.png';
 import mainModel2 from '@assets/foto/mainModel2.png';
 import mainModel3 from '@assets/foto/mainModel3.png';
 
-const fotos = [
-    {
-        id: 1,
-        foto: <img src={mainModel1} alt="fotoModel" />,
-    },
-    {
-        id: 2,
-        foto: <img src={mainModel2} alt="fotoModel" />,
-    },
-    {
-        id: 3,
-        foto: <img src={mainModel3} alt="fotoModel" />,
-    },
-];
 const HeaderBox = () => (
     <BoxContainer>
         <BoxFotos>
-            {fotos.map((i) => (
-                <div key={i.id}>{i.foto}</div>
-            ))}
+            <FotoModel1>
+                <img src={mainModel1} alt="fotoModel" />,
+            </FotoModel1>
+            <FotoModel2>
+                <img src={mainModel2} alt="fotoModel" />,
+                <BoxTitle>
+                    <NewCollection>Новая коллекция</NewCollection>
+                    <Line />
+                    <WatchProductsContainer>
+                        <WatchProducts>СМОТРЕТЬ НОВИНКИ</WatchProducts>
+                        <ArrowContainer>
+                            <ArrowLight />
+                        </ArrowContainer>
+                    </WatchProductsContainer>
+                </BoxTitle>
+            </FotoModel2>
+            <FotoModel3>
+                <img src={mainModel3} alt="fotoModel" />,
+            </FotoModel3>
         </BoxFotos>
-        <BoxTitle>
-            <NewCollection>Новая коллекция</NewCollection>
-            <Line />
-            <WatchProducts>Смотреть Новинки</WatchProducts>
-        </BoxTitle>
     </BoxContainer>
 );
 
--- src/components/Main/HeaderBox/styled.ts
+++ src/components/Main/HeaderBox/styled.ts
@@ -1,14 +1,39 @@
 import styled from 'styled-components';
+import Typography from '@components/Typography';
 
 export const BoxContainer = styled.div`
-    ${({ theme }) => theme.mixins.mainGrid};
     position: relative;
     bottom: 85px;
-    z-index: 1;
 `;
-export const BoxTitle = styled.div``;
-export const NewCollection = styled.div``;
-export const WatchProducts = styled.div``;
+
+export const BoxTitle = styled.div`
+    ${({ theme }) => theme.mixins.flexCenter};
+    position: absolute;
+    flex-direction: column;
+    width: 100%;
+    top: 400px;
+    left: 0px;
+    color: ${({ theme }) => theme.colors.white()};
+`;
+export const FotoModel1 = styled.image`
+    margin: 0 -5px;
+`;
+export const FotoModel2 = styled.image``;
+export const FotoModel3 = styled.image`
+    margin: 0 -5px;
+`;
+export const NewCollection = styled(Typography).attrs({ variant: 'dHeader46' })`
+    margin-bottom: 20px;
+`;
+export const WatchProducts = styled(Typography).attrs({ variant: 'dHeader16' })``;
+export const WatchProductsContainer = styled.button`
+    flex-direction: row;
+    margin-top: 20px;
+    ${({ theme }) => theme.mixins.flexCenter};
+    justify-content: center;
+    column-gap: 7px;
+`;
+export const ArrowContainer = styled.div``;
 export const BoxFotos = styled.div`
     ${({ theme }) => theme.mixins.flexCenterCenter};
     z-index: 1;
