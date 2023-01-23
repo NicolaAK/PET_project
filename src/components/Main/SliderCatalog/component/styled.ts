04e586acc94124d92cf250a41f8740a7b143aa9c
NikolaAK <kolyankalachev@gmail.com>
2023-01-23 17:35:58 +0700

    прочее

--- src/components/Main/SliderCatalog/component/styled.ts
+++ src/components/Main/SliderCatalog/component/styled.ts
@@ -17,8 +17,12 @@ export const ArrowContainerLeft = styled.div`
     height: 40px;
 `;
 export const ArrowBox = styled.button`
+    transition: ${({ theme }) => theme.decorations.transition};
     background: ${({ theme }) => theme.colors.mainPrimary()};
     ${({ theme }) => theme.mixins.flexCenterCenter};
     width: 100%;
     height: 100%;
+    &:hover {
+        transform: scale(1.1);
+    }
 `;
