a3228a9f92161be529566874325d2db4a92aa1a1
Дмитрий Сорокин <irondimk@gmail.com>
2023-03-11 14:24:21 +0700

    fix delay

--- scripts/server.js
+++ scripts/server.js
@@ -2,12 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(require('./db.js')());
 const middlewares = jsonServer.defaults();
-const randomInteger = (min, max) => {
-    const rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-};
+
 server.use((req, res, next) => {
-    setTimeout(next, randomInteger(400, 2000));
+    setTimeout(next, 400);
 });
 server.use(middlewares);
 server.use(router);
