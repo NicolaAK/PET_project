f46ac3bc84935818c3096bdd8fc529a1a1cf39ae
NikolaAK <kolyankalachev@gmail.com>
2023-04-06 15:30:17 +0700

    №1

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from '@components/Catalog/components/Menu';
 import Filter from '@components/Catalog/components/Filter';
 import Products from '@components/Catalog/components/Products';
 import Breadcrumbs from '@components/ReusedComponents/Breadcrumbs';
 import { CatalogContent, CatalogContainer, Container, Heading, Text } from './style';
 
-const Catalog = () => (
-    <CatalogContainer>
-        <CatalogContent>
-            <Breadcrumbs />
-            <Container>
-                <Heading>
-                    <Text>Каталог</Text>
-                </Heading>
-                <Menu />
-                <Filter />
-                <Products />
-            </Container>
-        </CatalogContent>
-    </CatalogContainer>
-);
+const Catalog = () => {
+    const [totalCount, setTotalCount] = useState(0);
+    return (
+        <CatalogContainer>
+            <CatalogContent>
+                <Breadcrumbs />
+                <Container>
+                    <Heading>
+                        <Text>Каталог</Text>
+                    </Heading>
+                    <Menu />
+                    <Filter />
+                    <Products />
+                </Container>
+            </CatalogContent>
+        </CatalogContainer>
+    );
+};
 
 export default Catalog;
--- src/store/product/http.ts
+++ src/store/product/http.ts
@@ -1,5 +1,7 @@
 import { http, ICommonResponse } from '@utils/http';
 import { IProduct } from '@store/product/types';
 
-export const fetchProductListRequest = () =>
-    http.get<void, ICommonResponse<Array<IProduct>>>('product?_page=1&_limit=20');
+export const fetchProductListRequest = (limit = 18, page = 1) =>
+    http.get<void, ICommonResponse<Array<IProduct>>>('product', {
+        params: { _limit: limit, _page: page },
+    });
