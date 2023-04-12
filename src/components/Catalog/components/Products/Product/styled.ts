6ba0ca1755a956245d3fac18e4a1ab506108585b
Дмитрий Сорокин <irondimk@gmail.com>
2023-04-13 00:27:36 +0700

    minor fix

--- src/components/Catalog/components/Products/Product/styled.ts
+++ src/components/Catalog/components/Products/Product/styled.ts
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
-interface IColor {
-    color: string;
-}
+
 interface IImageSize {
     heightImage?: number;
     widthImage?: number;
 }
+
 export const Container = styled.div`
     position: relative;
     display: inline-block;
     margin-bottom: 15px;
 `;
+
 export const Content = styled.div``;
+
 export const Photo = styled.button<IImageSize>`
     display: block;
     overflow: hidden;
     height: ${({ heightImage }) => heightImage}px;
 `;
+
 export const Image = styled.img<IImageSize>`
     width: ${({ widthImage }) => widthImage}px;
     transition: 0.7s;
@@ -24,6 +26,7 @@ export const Image = styled.img<IImageSize>`
         transform: scale(1.03);
     }
 `;
+
 export const Favourites = styled.div`
     position: absolute;
     top: 0;
@@ -34,6 +37,7 @@ export const Favourites = styled.div`
     border-radius: 0px 0px 0px 20px;
     z-index: 2;
 `;
+
 export const Description = styled.div`
     display: flex;
     flex-direction: column;
@@ -41,6 +45,7 @@ export const Description = styled.div`
     gap: 5px;
     padding-top: 10px;
 `;
+
 export const NameProduct = styled.button`
     ${({ theme }) => theme.helpers.getTypography('dText16lh19')}
     transition: ${({ theme }) => theme.decorations.transition};
@@ -52,33 +57,26 @@ export const NameProduct = styled.button`
         color: ${({ theme }) => theme.colors.buttonPrimaryClick()};
     }
 `;
+
 export const New = styled.div`
     ${({ theme }) => theme.helpers.getTypography('dText16lh19')}
     color: ${({ theme }) => theme.colors.elipsePastel()};
     position: absolute;
     right: 20%;
 `;
+
 export const Price = styled.div`
     ${({ theme }) => theme.helpers.getTypography('dTextBold16')}
 `;
+
 export const Sizes = styled.div`
     ${({ theme }) => theme.mixins.flexStartCenter};
     column-gap: 5px;
 `;
+
 export const Size = styled.div`
     ${({ theme }) => theme.helpers.getTypography('dText16lh19')}
     color: ${({ theme }) => theme.colors.blackPrimary(0.5)}
-`;
-export const Colors = styled.div`
-    ${({ theme }) => theme.mixins.flexStartCenter};
-    column-gap: 5px;
-`;
-export const EllipseColor = styled.div<IColor>`
-    width: 14px;
-    height: 14px;
-    background-color: ${({ color }) => color};
-    border-radius: 15px;
-    border: 1px solid ${({ theme, color }) => (color === '#FFFFFF' ? theme.colors.blackPrimary(0.5) : 'transparent')};
 `;
 
 export const Icon = styled.button`
