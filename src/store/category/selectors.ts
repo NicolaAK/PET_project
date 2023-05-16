78a2f7873ded425d42040a30240f5167804cef7b
NicolaAK <kolyankalachev@gmail.com>
2023-05-17 00:08:04 +0700

    Breadcrumbs #1

--- src/store/category/selectors.ts
+++ src/store/category/selectors.ts
@@ -3,8 +3,9 @@ import { categoryAdapter } from '@store/category/index';
 
 const getCategories = (state: RootState) => state.category;
 
-const { selectAll } = categoryAdapter.getSelectors(getCategories);
+const { selectAll, selectById } = categoryAdapter.getSelectors(getCategories);
 
 export const getCategoryList = (state: RootState) => selectAll(state);
+export const getCategoryById = (id: number) => (state: RootState) => selectById(state, id);
 export const getCategoryIsLoading = (state: RootState) => !!state.category.isLoading.length;
 export const getIsInitialized = (state: RootState) => state.category.isInitialized;
--- src/store/product/selectors.ts
+++ src/store/product/selectors.ts
@@ -3,9 +3,10 @@ import { productAdapter } from '@store/product/index';
 
 const getProducts = (state: RootState) => state.product;
 
-const { selectAll } = productAdapter.getSelectors(getProducts);
+const { selectAll, selectById } = productAdapter.getSelectors(getProducts);
 
 export const getProductList = (state: RootState) => selectAll(state);
+export const getProductById = (id: number) => (state: RootState) => selectById(state, id);
 export const getProductIsLoading = (state: RootState) => !!state.product.isLoading.length;
 export const getIsInitialized = (state: RootState) => state.product.isInitialized;
 export const getProductTotalCount = (state: RootState) => state.product.totalCount;
